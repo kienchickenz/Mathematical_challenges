9ee95972915e1a9c6d0dc28e81fd70d2f6b0cc19
kienchickenz <91973269+kienchickenz@users.noreply.github.com>
2022-05-31 22:29:34 +0700

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include "Fibonacci.hpp"
+#include<vector>
 
 int main() {
 
@@ -8,6 +8,17 @@ int main() {
 	std::cout << "Welcome to the Fibonacci sequence,\n";
 	std::cout << "Enter the n(th): ";
 	std::cin >> n;
+	
+	std::vector<int> sequence(n);
+
+	sequence[0] = 1;
+	sequence[1] = 1;
+
+	for(int i = 2; i < n; i++) {
+
+		sequence[i] = sequence[i - 1] + sequence[i - 2];
+
+	}
 
 	std::cout << "\nThe " << n << "th number is " << Fibonacci(n);
 
