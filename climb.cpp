381c005a81e196ac19e7f9d67db83aa1918ef59b
Nguyen Duc Kien <91973269+kienchickenz@users.noreply.github.com>
2022-06-23 23:07:28 +0700

    Add files via upload

---
+++ climb.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	int n;
+
+	cout << "Enter the n: ";
+	cin >> n;
+
+	vector<int> number(n);
+	number[0] = 1;
+	number[1] = 1;
+
+	for(int i = 2; i < n; i++) {
+
+		number[i] = number[i - 1] + number[i - 2];
+
+	}
+
+	cout << "The " << n << "th number is: " << number[n - 1];
+
+}
