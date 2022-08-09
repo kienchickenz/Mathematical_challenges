91208366d20835102684b283cea0a19e56d58259
Nguyen Duc Kien <91973269+kienchickenz@users.noreply.github.com>
2022-08-09 16:06:36 +0700

    Update Knapsack.py

--- Knapsack.py
+++ Knapsack.py
@@ -2,6 +2,7 @@ my_weight = [4, 3, 1, 2, 3]
 my_value = [5, 4, 2, 3, 2]
 limit = 7
 
+# Based on the matrix
 def knapsack(my_weight, my_value, limit):
 
     my_row = len(my_weight) + 1 # = 6
@@ -10,6 +11,7 @@ def knapsack(my_weight, my_value, limit):
     weight_result = []
     value_result = []
     
+    # Create the matrix 
     my_matrix = [[0 for col in range(my_col)] for row in range(my_row)]
 
     for row in range(1, my_row):
@@ -23,7 +25,8 @@ def knapsack(my_weight, my_value, limit):
                 if my_matrix[row - 1][col - my_weight[row - 1]] + my_value[row - 1] > my_matrix[row - 1][col]:
 
                     my_matrix[row][col] = my_matrix[row - 1][col - my_weight[row - 1]] + my_value[row - 1]
-            
+    
+    # Decide which things to take (start from bottom right of the matrix to the top left corner)
     final_col = limit
     
     for row in range(my_row - 1, 0, -1):
